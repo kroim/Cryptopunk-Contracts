69f52754a2774fc25c8503a83c399fe5e52a90ac
kroim <wonderfulplayer@outlook.com>
2021-08-24 01:50:49 +0800

    Update CryptoPig.sol

--- contracts/CryptoPig.sol
+++ contracts/CryptoPig.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract CryptoPig is ERC721, Ownable {
+contract CryptoPig is ERC721, Ownable, ReentrancyGuard {
     using SafeMath for uint256;
 
     uint256 private _punkIndex = 0;
@@ -13,16 +14,16 @@ contract CryptoPig is ERC721, Ownable {
     uint256 public currentSupply = 0;
     uint256 public maxPunksPurchase = 10;
     uint256 public mintPrice = 500 ether;  // 500 ONE
-    uint256 private _devXFee = 20;  // 20%
-    uint256 private _devYFee = 75;  // 75%
-    address private _devWalletX;
-    address private _devWalletY;
-    uint256 private _devXBalance = 0;
-    uint256 private _devYBalance = 0;
+    uint256 public _devXFee = 20;  // 20%
+    uint256 public _devYFee = 75;  // 75%
+    address public _devWalletX;
+    address public _devWalletY;
+    uint256 public _devXBalance = 0;
+    uint256 public _devYBalance = 0;
     uint256 public reflectionFee = 5;  // 5%
     uint256 public totalReflectionBalance = 0;
 
-    bool paused = true;
+    uint256 public mintablePaused = 0;
 
     struct Punk {
         address _pCreator;
@@ -57,26 +58,24 @@ contract CryptoPig is ERC721, Ownable {
         _devWalletY = _devY;
     }
 
-    function withdrawDevX() public {
+    function withdrawDevX() public nonReentrant {
         require(msg.sender == _devWalletX, "You are not DevX!");
-        uint256 balanceX = _devXBalance;
+        payable(msg.sender).transfer(_devXBalance);
         _devXBalance = 0;
-        payable(msg.sender).transfer(balanceX);
     }
 
-    function withdrawDevY() public {
+    function withdrawDevY() public nonReentrant {
         require(msg.sender == _devWalletY, "You are not DevY!");
-        uint256 balanceY = _devYBalance;
+        payable(msg.sender).transfer(_devYBalance);
         _devYBalance = 0;
-        payable(msg.sender).transfer(balanceY);
     }
 
     function setMaxPunksPurchase(uint256 _maxPunksPurchase) public onlyOwner {
         maxPunksPurchase = _maxPunksPurchase;
     }
 
-    function setPaused(bool _pause) public onlyOwner {
-        paused = _pause;
+    function setPaused(uint256 _mintablePaused) public onlyOwner {
+        mintablePaused = _mintablePaused;
     }
 
     function tokenURI(uint256 _tokenId) override public view returns (string memory) {
@@ -96,22 +95,23 @@ contract CryptoPig is ERC721, Ownable {
         isMinter[_minterAddress] = false;
     }
 
-    function mintPunk(uint256 _numberOf, string[] memory _uris) public payable {
-        require(_numberOf > 0, "Empty data");
-        require(_uris.length == _numberOf, "Invalid data");
+    function mintPunk(string memory _uri) public payable {
+        require(mintablePaused == 1, "Minting is paused");
+        uint256 _numberOf = 1;
         require(currentSupply + _numberOf <= totalSupply, "No punks available for minting!");
         if (_msgSender() != owner()) {
             require(msg.value >= mintPrice.mul(_numberOf), "insufficient balance");
         }
         currentSupply += _numberOf;
         _pBalanceOf[msg.sender] = _numberOf;
+        splitBalance(msg.value);
         for (uint256 i = 0; i < _numberOf; i++) {
             uint256 _prevPunkIndex = _punkIndex;
             _punkIndex = _punkIndex.add(1);
             Punk storage newPunk = punks[_prevPunkIndex];
             newPunk._pCreator = msg.sender;
             newPunk._pOwner = msg.sender;
-            newPunk._pUri = _uris[i];
+            newPunk._pUri = _uri;
             newPunk._tokenId = _prevPunkIndex;
             newPunk._pOwnershipRecords.push(msg.sender);
             _safeMint(msg.sender, _prevPunkIndex);
@@ -139,6 +139,7 @@ contract CryptoPig is ERC721, Ownable {
         the user can claim the rest amount continue. If less, the user can't claim
      */
     function claimReflection() public onlyHolder {
+        require(mintablePaused != 1, "You can claim your reward after Minting has done!");
         uint256 withdrawedAmount = _pWidthraws[msg.sender];
         uint256 claimAmount = totalReflectionBalance.mul(_pBalanceOf[msg.sender]).div(currentSupply);
         require(withdrawedAmount < claimAmount, "You already claimed your reflection!");
