21c53fa946b9a84dc03108ed1b95cc8b38c2ee93
kroim <wonderfulplayer@outlook.com>
2021-12-06 17:41:18 +0800

    Update FVT-flat.sol

--- contracts/FVT-flat.sol
+++ contracts/FVT-flat.sol
@@ -1262,7 +1262,7 @@ library CustomString {
 contract FunkiVIPToken is ERC721, Ownable {
     using SafeMath for uint256;
 
-    string public VIP_PROVENANCE = "";
+    string public FVT_PROVENANCE = "";
     uint256 public currentSupply = 0;
     uint256 public totalSupply = 1000;
     uint256 public mintPrice = 0.1 ether;
@@ -1272,15 +1272,15 @@ contract FunkiVIPToken is ERC721, Ownable {
     uint256 public maxMintNumber = 10;
     bool public paused = false;
 
-    struct VIP {
+    struct FVT {
         uint256 tokenId;
         address creator;
         address owner;
         string uri;
     }
-    mapping(uint256=>VIP) private vips;
+    mapping(uint256=>FVT) private fvts;
 
-    event VIPMint(address indexed to, VIP _vip);
+    event FVTMint(address indexed to, FVT _fvt);
     event SetPrice(uint256 _value);
     event ChangePaused(bool _value);
     
@@ -1307,31 +1307,31 @@ contract FunkiVIPToken is ERC721, Ownable {
     }
 
     function setProvenanceHash(string memory provenanceHash) public onlyOwner {
-        VIP_PROVENANCE = provenanceHash;
+        FVT_PROVENANCE = provenanceHash;
     }
 
-    function mintVIP(uint256 _numberOfTokens) public payable {
+    function mintFVT(uint256 _numberOfTokens) public payable {
         require(!paused, "Minting is paused");
         require(_numberOfTokens == 1, "Too many tokens to mint at once.");
-        require(currentSupply.add(_numberOfTokens) < totalSupply, "No vip available for minting!");
+        require(currentSupply.add(_numberOfTokens) < totalSupply, "No fvt available for minting!");
         require(msg.value >= mintPrice.mul(_numberOfTokens), "Amount is not enough!");
 
         uint256 _index = currentSupply;
         currentSupply += _numberOfTokens;
         _tokenBalance[_msgSender()] += _numberOfTokens;
         for (uint256 i = 0; i < _numberOfTokens; i++) {
-            VIP storage newVIP = vips[_index.add(i)];
-            newVIP.tokenId = _index.add(i);
-            newVIP.creator = _msgSender();
-            newVIP.owner = _msgSender();
-            newVIP.uri = createTokenURI(_index.add(i));
+            FVT storage newFVT = fvts[_index.add(i)];
+            newFVT.tokenId = _index.add(i);
+            newFVT.creator = _msgSender();
+            newFVT.owner = _msgSender();
+            newFVT.uri = createTokenURI(_index.add(i));
             _safeMint(_msgSender(), _index.add(i));
-            emit VIPMint(_msgSender(), newVIP);
+            emit FVTMint(_msgSender(), newFVT);
         }
     }
     
     function creatorOf(uint256 _tokenId) public view returns (address) {
-        return vips[_tokenId].creator;
+        return fvts[_tokenId].creator;
     }
 
     function balanceOf(address account) public view virtual override returns(uint256) {
@@ -1340,7 +1340,7 @@ contract FunkiVIPToken is ERC721, Ownable {
 
     function tokenURI(uint256 _tokenId) override public view returns (string memory) {
         require(_tokenId < currentSupply, "ERC721Metadata: URI query for nonexistent token");
-        return vips[_tokenId].uri;
+        return fvts[_tokenId].uri;
     }
 
     function withdrawAll() public onlyOwner {
@@ -1348,20 +1348,20 @@ contract FunkiVIPToken is ERC721, Ownable {
     }
 
     function ownerOf(uint256 tokenId) public view virtual override returns (address) {
-        address owner = vips[tokenId].owner;
+        address owner = fvts[tokenId].owner;
         require(owner != address(0), "ERC721: owner query for nonexistent token");
         return owner;
     }
 
     function _transfer(address from, address to, uint256 tokenId) internal virtual override {
-        // check vip index is available
+        // check token index is available
         require(tokenId < currentSupply, "Undefined tokenID!");
-        // check owner of vip
+        // check owner of token
         require(ownerOf(tokenId) == from, "Caller is not owner");
         require(to != address(0), "ERC721: transfer to the zero address");
         _beforeTokenTransfer(from, to, tokenId);
         _approve(address(0), tokenId);
-        vips[tokenId].owner = to;
+        fvts[tokenId].owner = to;
         _tokenBalance[from]--;
         _tokenBalance[to]++;
         emit Transfer(from, to, tokenId);
